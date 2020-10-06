2f89e81df7b7fe518d639941231f3c151b64007b
Peter Nguyen <ppn4@txstate.edu>
2020-10-06 01:04:25 +0000

    Initial commit

---
+++ hw5.cpp
@@ -0,0 +1,114 @@
+/***
+* PSUEDOCODE
+  BubbleSort (vector string &vec)
+    for i less than vec.size
+      display vec[i]
+    for i < than vec.size
+    for j < than vec.size
+      if vec[i].compare(vec[i+1])
+        if vec[i] is  > than vec[j]
+          swap vec[i],vec[j]
+        else if vec[i] is < than vec[j]
+          swap vec[i], vec[j]
+-----------------------------------------------
+  void InsertionSortHelper (vector<int>&v, in n)
+    initialize temp to v[n-1]
+    initialize index to n - 2
+
+    if n is > than 0
+      call InsertionSortHelper 
+    while index >= 0 && temp < v[index]
+      v[index +1] is = to v[index]
+      decrement index by 1
+    initialize temp to temp
+  
+  void InsertionSort(vector<int>&v)
+    call InsertionSortHelper
+-----------------------------------------------
+  void Merge (vector<string> &vec, int low, int high)
+    initialize mid to (low + high) divided by 2
+    initialize i to low
+    initialize j to mid + 1 
+    initialize k to low
+
+
+  
+***/
+
+#include <iostream>
+#include <vector>
+#include "hw5.h"
+
+void BubbleSort(std::vector<std::string> &vec) {
+  for (int i = 0; i < vec.size(); i++)
+  std::cout << vec[i] << ' ';
+
+  for (int i = 0; i < vec.size(); i++) {
+    for (int j = i + 1; j < vec.size(); j++) 
+    {
+      if (vec[i].compare(vec[i+1])) {
+        if (vec[i] > vec[j])
+          swap(vec[i],vec[j]);
+      }
+        else if (vec[i] < vec[j])
+          swap(vec[i],vec[j]);
+    }
+  }
+}
+
+void InsertionSortHelper(std::vector<int>&v, int n) {
+  int temp = v[n-1];
+  int index = n - 2;
+
+  if(n > 0) 
+    InsertionSortHelper(v, n - 1);
+  
+  while(index >= 0 && temp < v[index]) {
+    v[index + 1] = v[index];
+    index--;
+  }
+  v[index + 1] = temp;
+}
+
+void InsertionSort(std::vector<int>&v) {
+  InsertionSortHelper(v, v.size());
+}
+
+void Merge(std::vector<std::string> &vec, int low, int high) {
+  int mid = (low + high)/2;
+  int i = low;
+  int j = mid + 1;
+  int k = low;
+
+  std::vector<std::string> temp(vec.size());
+
+  while(i <= mid && j <= high) {
+    if(vec[i] < vec[j]) 
+      temp[k++] = vec[i++];
+    else 
+      temp[k++] = vec[i++];
+  }
+  
+  while(i <= mid) 
+    temp[k++] = vec[i++];
+  while(j <= high) 
+    temp[k++] = vec[j++];
+  for(int i = low; i <= high; i++) 
+    vec[i]= temp[i];
+}
+
+void MergeSortHelper(std::vector<std::string> &vec,int low, int high) {
+  int middle;
+    if (low >= high)
+    return;
+
+    middle = (low + high) / 2;
+    MergeSortHelper(vec, low, middle);
+    MergeSortHelper(vec, middle + 1, high);
+    Merge(vec, low, high); 
+}
+
+void MergeSort(std::vector<std::string> &vec) {
+  int n = vec.size();
+  MergeSortHelper(vec, 0 , n-1);
+}
---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include "hw5.h"
+
+void print(std::vector<std::string> &v) {
+  std::cout << "[";
+  bool first = true;
+  for (auto x : v) {
+    if (!first) {
+      std::cout << ", ";
+    }
+    std::cout << x;
+    first = false;
+  }
+  std::cout << "]";
+  std::cout << std::endl;
+}
+
+void print2(std::vector<int> &v) {
+  std::cout << "[";
+  bool first = true;
+  for (auto x : v) {
+    if (!first) {
+      std::cout << ", ";
+    }
+    std::cout << x;
+    first = false;
+  }
+  std::cout << "]";
+  std::cout << std::endl;
+}
+
+int main() {
+  std::vector<std::string> v;
+  std::vector<std::string> k;
+
+
+  v = {"emma", "dora", "ruth", "ziegellaub", "eichler"};
+  std::cout << "Case 1: Bubble Sort" << std::endl;
+  BubbleSort(v);
+  std::cout << std::endl;
+  print(v);
+  std::cout << std::endl << std::endl;
+
+  std::vector<int> vec;
+  MergeSort(v);
+  std::cout << "Case 2: Insertion Sort " << std::endl;
+  //vec = {11, 20, 24, 8, 10, 1, 3, 4, 34, 14};
+  vec = {-1, 0,34, 65, 79, 56, 65, 9, 2, 5, 1000, 101};
+  InsertionSort(vec);
+  print2(vec);
+  std::cout << std::endl;
+  
+
+  //k = {"Kobe", "Balls", "Jenkins", "Leeroy", "Ligma", "Bryant"};
+  k = {"Jared", "dora", "Jace", "Sam", "eichler"};
+  MergeSort(k);
+  std::cout << "Case 3: MergeSort " << std::endl;
+  print(k);
+  //std::cout << "Expected: [dora, eichler, emma, ruth, ziegellaub]" << std::endl;
+
+  //blah
+}
